48d6bf92b240cec00cddfc5d5f2a8ee21db96de9
Juan Arias <juan.arias.freire@gmail.com>
2016-04-18 23:00:36 +0200

    It works now, by adding proper styles + media query

--- generate.sh
+++ generate.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-TOPDIR=$(cd $(dirname $0) ; pwd);
-
-[ $# -ne 1 ] && \
-  echo "Missing parameter: images folder." && \
-  exit 2
-
-TEMPLATE_FILE="$TOPDIR/templates/section.tmpl"
-IMAGE_FOLDER=$(cd "$1" ; pwd)
-MAIN_FILE="${TOPDIR}/section.html"
 
 sub() {
   local this=$1
@@ -27,6 +18,14 @@ insert_image() {
 
   sub "#IMAGE${image_index}_SRC" "${image}" "$index_file"
   sub "#IMAGE${image_index}_TEXT" "${image_name}" "$index_file"
+
+  if [ ! -z "$ADJUST_ORIENTATION" ] ; then
+    # 6 means the image is rotated
+    local orientation=$(identify -format '%[EXIF:Orientation]' "$image" )
+    image_class='image-landscape'
+    [ $orientation -eq 6 ] && image_class="image-left"
+    sub "#IMAGE${image_index}_CLASS" "${image_class}" "$index_file"
+  fi
 }
 
 insert_description() {
@@ -38,50 +37,71 @@ insert_description() {
   sub "#DESCRIPTION" "${escaped_description}" "$index_file"
 }
 
+process_dir() {
+  local title=$(basename "$dir")
+  local invalid_folder=0
+  local index_file="$(mktemp /tmp/.index.html.XXXXX)"
 
-> "$MAIN_FILE"
-
-find "$IMAGE_FOLDER"  -type d  -print0 | while IFS= read -r -d '' dir ; do
-  if [ "$dir" = "$IMAGE_FOLDER" ] ; then
-    continue
-  fi
-
-  title=$(basename "$dir")
-  invalid_folder=0
-  index_file="$(mktemp /tmp/.index.html.XXXXX)"
+  # copy the template into a new file
   cat "$TEMPLATE_FILE" > "$index_file"
-
-  find "$dir" -type f  -print0 | while IFS= read -r -d '' file ; do
+  find "$dir" -maxdepth 1 -type f  -print0 | while IFS= read -r -d '' file ; do
     echo "$file" | grep -qi "~" && continue
 
     file_info=$(file "$file")
-    echo "$file_info" | grep -qi image && insert_image "$file" "$index_file" && continue
-    echo "$file_info" | grep -qi text && insert_description "$file" "$index_file" && continue
-    
+    echo "$file_info" | grep -qi "image data" && insert_image "$file" "$index_file" && continue
+    echo "$file_info" | grep -qi "text" && insert_description "$file" "$index_file" && continue
+
     # Ooops, shouldn't be here
-    echo "Folder contains no valid data: $file" && invalid_folder=1
+    echo "Folder '$dir' contains no valid data: $file" && return 
   done
 
-  if [ $invalid_folder -eq 1 ] ; then
-    echo "Folder contains no valid data: $dir"
-  else 
-    sub "#TITLE" "${title}" "$index_file"
-    
-    cat "$index_file"  >> "$MAIN_FILE"
-    rm -f "$index_file"
-  fi
-done
+  sub "#TITLE" "${title}" "$index_file"
+  cat "$index_file"  >> "$MAIN_FILE"
+  rm -f "$index_file"
+}
 
-# Create FINAL file
-FINAL_FILE="${TOPDIR}/index.html"
-START_TEMPLATE="$TOPDIR/templates/start.tmpl"
-END_TEMPLATE="$TOPDIR/templates/ending.tmpl"
-cat > "$FINAL_FILE" << EOF
-$(cat "$START_TEMPLATE")
-$(cat "$MAIN_FILE")
-$(cat "$END_TEMPLATE")
+process_image_folder() {
+  #find "$IMAGE_FOLDER"  -type d -print0 | while IFS= read -r -d '' dir ; do
+  local tmp_file=$(mktemp  /tmp/.tmpdirXXXX)
+  IFS=$';'
+  find "$IMAGE_FOLDER"  -type d | sort -h > "$tmp_file"
+  unset IFS
+  while read dir ; do
+    [ $(find "$dir" -maxdepth 1 -type f | wc -l) -gt 0 ] && \
+      process_dir "$dir"
+  done < "$tmp_file"
+  rm -rf "$tmp_file"
+}
+
+create_final_file() {
+  FINAL_FILE="${TOPDIR}/index.html"
+  START_TEMPLATE="$TOPDIR/templates/start.tmpl"
+  END_TEMPLATE="$TOPDIR/templates/ending.tmpl"
+  cat > "$FINAL_FILE" << EOF
+  $(cat "$START_TEMPLATE")
+  $(cat "$MAIN_FILE")
+  $(cat "$END_TEMPLATE")
 EOF
 
-# REMOVE temps
-rm -rf "$MAIN_FILE"
-echo "Done! Created: $FINAL_FILE"
+  # REMOVE temps
+  rm -rf "$MAIN_FILE"
+  echo "Done! Created: $FINAL_FILE"
+}
+
+# Check params
+[ $# -ne 1 ] && \
+  echo "Missing parameter: images folder." && \
+  exit 2
+
+[ -z $(which identify) -a ! -z "$ADJUST_ORIENTATION" ] &&
+  echo "Missing command: 'identify'. Please install ImageMagick" && \
+  exit 2
+
+TOPDIR=$(cd $(dirname $0) ; pwd);
+TEMPLATE_FILE="$TOPDIR/templates/section.tmpl"
+IMAGE_FOLDER=$(cd "$1" ; pwd)
+MAIN_FILE="${TOPDIR}/section.html"
+> "$MAIN_FILE"
+
+process_image_folder
+create_final_file
