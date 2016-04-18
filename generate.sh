dd3a8582feed1bf6964aba85b9e12e02df94a992
Juan Arias <juan.arias.freire@gmail.com>
2016-04-18 21:10:58 +0200

    Generate a single, huge, ugly file

--- generate.sh
+++ generate.sh
@@ -5,8 +5,9 @@ TOPDIR=$(cd $(dirname $0) ; pwd);
   echo "Missing parameter: images folder." && \
   exit 2
 
-TEMPLATE_FILE="$TOPDIR/templates/index.html.tmpl"
+TEMPLATE_FILE="$TOPDIR/templates/section.tmpl"
 IMAGE_FOLDER=$(cd "$1" ; pwd)
+MAIN_FILE="${TOPDIR}/section.html"
 
 sub() {
   local this=$1
@@ -20,11 +21,11 @@ insert_image() {
   local image=$1
   local index_file=$2
 
-  image_src=$(basename "$image")
-  image_index=$(echo $image_src | grep -o "^[1-6]")
-  image_name=$(echo $image_src | sed -e 's#^[1-6]\s*##g' -e 's#\.[a-zA-Z]*$##g')
+  image_base=$(basename "$image")
+  image_index=$(echo $image_base | grep -o "^[1-6]")
+  image_name=$(echo $image_base | sed -e 's#^[^a-zA-Z]*##g' -e 's/.jpg//g' -e 's/.JPG//g' )
 
-  sub "#IMAGE${image_index}_SRC" "${image_src}" "$index_file"
+  sub "#IMAGE${image_index}_SRC" "${image}" "$index_file"
   sub "#IMAGE${image_index}_TEXT" "${image_name}" "$index_file"
 }
 
@@ -35,25 +36,22 @@ insert_description() {
   descripcion="$(cat "$description_file")"
   escaped_description="$(echo "${descripcion}" | sed ':a;N;$!ba;s/\n/<br \/>/g' | sed 's/\$/\\$/g')"
   sub "#DESCRIPTION" "${escaped_description}" "$index_file"
-  
 }
+
+
+> "$MAIN_FILE"
 
 find "$IMAGE_FOLDER"  -type d  -print0 | while IFS= read -r -d '' dir ; do
   if [ "$dir" = "$IMAGE_FOLDER" ] ; then
     continue
   fi
 
-  # Cleanup old HTML
-  find "$dir" -iname "*.html*" -delete
-
   title=$(basename "$dir")
   invalid_folder=0
   index_file="$(mktemp /tmp/.index.html.XXXXX)"
   cat "$TEMPLATE_FILE" > "$index_file"
 
-  pushd "$dir" > /dev/null
-  find . -type f  -name "*.html" -delete
-  find . -type f  -print0 | while IFS= read -r -d '' file ; do
+  find "$dir" -type f  -print0 | while IFS= read -r -d '' file ; do
     echo "$file" | grep -qi "~" && continue
 
     file_info=$(file "$file")
@@ -65,15 +63,25 @@ find "$IMAGE_FOLDER"  -type d  -print0 | while IFS= read -r -d '' dir ; do
   done
 
   if [ $invalid_folder -eq 1 ] ; then
-    echo "Folder contains no valid data"
+    echo "Folder contains no valid data: $dir"
   else 
     sub "#TITLE" "${title}" "$index_file"
     
-    final_index_file="$dir/index.html"
-    mv "$index_file" "$final_index_file"
-    echo "Generada web para '$title' en '$final_index_file' ... "
+    cat "$index_file"  >> "$MAIN_FILE"
+    rm -f "$index_file"
   fi
-    
-  popd > /dev/null
-
 done
+
+# Create FINAL file
+FINAL_FILE="${TOPDIR}/index.html"
+START_TEMPLATE="$TOPDIR/templates/start.tmpl"
+END_TEMPLATE="$TOPDIR/templates/ending.tmpl"
+cat > "$FINAL_FILE" << EOF
+$(cat "$START_TEMPLATE")
+$(cat "$MAIN_FILE")
+$(cat "$END_TEMPLATE")
+EOF
+
+# REMOVE temps
+rm -rf "$MAIN_FILE"
+echo "Done! Created: $FINAL_FILE"
